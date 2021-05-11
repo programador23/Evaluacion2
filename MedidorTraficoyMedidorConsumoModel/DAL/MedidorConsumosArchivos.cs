af4e831bceec0a6831c577c1f98989d48c586c70
Christian Angulo <christiananguloprogramador23@gmail.com>
2021-05-11 12:33:53 -0400

    Metodo saveConsumo

---
+++ MedidorTraficoyMedidorConsumoModel/DAL/MedidorConsumosArchivos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MedidorTraficoyMedidorConsumoModel.DTO;
+
+namespace MedidorTraficoyMedidorConsumoModel.DAL
+{
+    public class MedidorConsumosArchivos : IMedidorConsumosDAL
+    {
+        private string archivo = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + "consumos.txt";
+        public List<MedidorConsumo> GetAll()
+        {
+            List<MedidorConsumo> medidorconsumos = new List<MedidorConsumo>();
+
+
+            return medidorconsumos;
+        }
+
+        public void savecosumos(MedidorConsumo m)
+        {
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(archivo, true))
+                {
+                    writer.WriteLine(m);
+                    writer.Flush();
+                }
+
+            }
+            catch (IOException ex)
+            {
+
+            }
+        }
+    }
+}
---
+++ MedidorTraficoyMedidorConsumoModel/DTO/MedidorConsumo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedidorTraficoyMedidorConsumoModel.DTO
+{
+    public class MedidorConsumo
+    {
+        private string id;
+        private string consumo;
+
+        public string Id { get => id; set => id = value; }
+        public string Consumo { get => consumo; set => consumo = value; }
+
+        public override string ToString()
+        {
+            return Id + ";" + Consumo;
+        }
+    }
+}
